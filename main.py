c3b098b5f7541b818e5f5d474538d0febbe495f5
pauloht <darktiger.p@gmail.com>
2022-01-17 20:27:37 -0300

    Commit Diario
    UI Basic(btn, action)
    Take screenshot, move mouse(with pyautogui)

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("This line will be printed.")
+from tkinter import *
+import pyautogui
+
+menu_inicial = Tk();
+menu_inicial.title("Primeira APP");
+menu_inicial.geometry(
+    "500x250");
+
+def printSS():
+    im2 = pyautogui.screenshot('my_screenshot.png');
+
+def moveMouse():
+    pyautogui.moveTo(50, 50);
+
+btnPrint = Button(
+    master = menu_inicial,
+    text = "Print",
+    command = printSS);
+btnMoveMouse = Button(
+    master = menu_inicial,
+    text = "Mover mouse",
+    command = moveMouse);
+btnPrint.pack();
+btnMoveMouse.pack();
+
+menu_inicial['bg'] = "gray"
+menu_inicial.mainloop();
